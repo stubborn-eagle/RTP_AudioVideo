b19d3a9684ebbf50cb5caf86dd23dea3c94a9a9f
Fulvio Di Maria <fulvio.dimaria@hotmail.it>
2014-01-10 15:01:56 +0100

    aggiunto il TestQuickCamPro

---
+++ test_vari/src/com/stubborneagle/AudioVideoFromWebcam/Stdout.java
@@ -0,0 +1,40 @@
+package com.stubborneagle.AudioVideoFromWebcam;
+
+/******************************************************
+ * File: Stdout.java.java
+ * created 24.07.2001 21:44:46 by David Fischer, fischer@d-fischer.com
+ * Description: utility class for standard output
+ */
+
+
+public class Stdout
+{
+
+	public static void log(String msg)
+	{
+		System.out.println(msg);
+	}
+
+
+	public static void logAndAbortException(Exception e)
+	{
+		log("" + e);
+		flush();
+		System.exit(0);
+	}
+
+
+	public static void logAndAbortError(Error e)
+	{
+		log("" + e);
+		flush();
+		System.exit(0);
+	}
+
+
+	public static void flush()
+	{
+		System.out.flush();
+	}
+
+}
